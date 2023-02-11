6ff23ddbcb955fc7a9a3f7696961136d8c3baefd
VedikaSrivastava <vedikas3012@gmail.com>
2023-02-10 19:18:55 -0500

    adding draft

--- app/src/main/java/com/example/temperatureconverter/TempConverterModel.kt
+++ app/src/main/java/com/example/temperatureconverter/TempConverterModel.kt
@@ -8,7 +8,14 @@ class TempConverterModel {
         return (celsius * 9/5) + 32
     }
 
-    fun fahrenheitToCelsius(fahrenheit: Float): Float {
+    fun fahrenheitToCelsius(fahrenheit: Int): Float {
         return (fahrenheit - 32) * 5/9
     }
 }
+
+/**
+class TemperatureConverter {
+    fun celsiusToFahrenheit(celsius: Int) = (celsius * 9 / 5) + 32
+    fun fahrenheitToCelsius(fahrenheit: Int) = (fahrenheit - 32) * 5 / 9
+}
+        */
